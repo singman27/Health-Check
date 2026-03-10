7d10dd409fa35e82dbcb9f682ba7d635c19bdbb6
singman27 <singhal.arun@gmail.com>
2026-03-10 14:31:45 +0800

    Update entrypoint.sh

--- bloodproject/entrypoint.sh
+++ bloodproject/entrypoint.sh
@@ -11,7 +11,22 @@ python manage.py wait_for_db
 # Run database migrations
 echo "Running database migrations..."
 python manage.py migrate 
-python manage.py createsuperuser --noinput || true
+python manage.py shell -c "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+import os
+pwd = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'admin123')
+try:
+    user = User.objects.get(username='admin')
+    user.is_staff = True
+    user.is_superuser = True
+    user.set_password(pwd)
+    user.save()
+    print('Superuser updated')
+except User.DoesNotExist:
+    User.objects.create_superuser('admin', 'adamritch172@gmail.com', pwd)
+    print('Superuser created')
+" || true
 
 # Collect static files
 echo "Collecting static files..."
